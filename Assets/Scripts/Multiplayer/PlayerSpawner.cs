2383528b828ed090b725565228388b549610ad06
JamieSchooling <99397650+JamieSchooling@users.noreply.github.com>
2025-12-24 21:44:14 +0000

    fix: Players not spawning on client

--- Assets/Scripts/Multiplayer/PlayerSpawner.cs
+++ Assets/Scripts/Multiplayer/PlayerSpawner.cs
@@ -8,6 +8,17 @@ public class PlayerSpawner : NetworkBehaviour
 
     public override void OnNetworkSpawn()
     {
+        NetworkManager.Singleton.SceneManager.OnSceneEvent += (sceneEvent) =>
+        {
+            if (sceneEvent.SceneEventType == SceneEventType.LoadEventCompleted)
+            {
+                SpawnPlayers();
+            }
+        };
+    }
+
+    public void SpawnPlayers()
+    {
         foreach (PlayerState p in ServerConnectionRegistry.Instance.AllPlayerStates)
         {
            NetworkObject.InstantiateAndSpawn
